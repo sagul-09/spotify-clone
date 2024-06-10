fcb910bcd5d55cc4a9fc437809ef16685d3fd653
sagul-09 <nihal.evil.royal@gmail.com>
2024-06-10 19:08:11 +0530

    dispaying album and song items are done

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
 import AlbumItem from './AlbumItem'
+import SongItem from './SongItem'
 import { albumsData } from '../assets/assets'
+import { songsData } from '../assets/assets'
 
 const DisplayHome = () => {
   return (
@@ -18,7 +20,9 @@ const DisplayHome = () => {
     <div className="mb-4 ">
         <h1 className='my-5 font-bold text-[24px]'>Today's Biggest Hits</h1>
         <div className="flex overflow-auto">
-        {}
+        {songsData.map((item,index)=>(
+          <SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
+        ))}
         </div>
     </div>
     </>
