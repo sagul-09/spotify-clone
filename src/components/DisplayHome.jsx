29a6e0e70d1e8c7dfc7e2a82f5a30c37c12d74cc
sagul-09 <nihal.evil.royal@gmail.com>
2024-06-10 17:31:49 +0530

    chore: Update DisplayHome component to render albums dynamically and hiddent  scorllvar in the overflow for the albumdata

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -8,11 +8,18 @@ const DisplayHome = () => {
     <>
     <Navbar/>
     <div className="mb-4 ">
-        <h1 className='my-5 font-bold text-[24px]'>Featured Charts</h1>\
-        <div className="flex overflow-auto"></div>
+        <h1 className='my-5 font-bold text-[24px]'>Featured Charts</h1>
+        <div className="flex overflow-auto">
         {albumsData.map((item,index)=>(
             <AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>
         ))}
+        </div>
+    </div>
+    <div className="mb-4 ">
+        <h1 className='my-5 font-bold text-[24px]'>Today's Biggest Hits</h1>
+        <div className="flex overflow-auto">
+        {}
+        </div>
     </div>
     </>
   )
